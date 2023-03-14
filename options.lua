c80e84b86499685a86b666ac4b7e6fcfcf7d9cf3
lch <lchopn@gmail.com>
2023-03-14 13:12:40 +0800

    Initial commit

---
+++ options.lua
@@ -0,0 +1,12 @@
+return {
+  opt = {
+    relativenumber = false,
+    expandtab = false,
+    tabstop = 4,
+    shiftwidth = 4,
+    softtabstop = 4,
+  },
+  g = {
+    python_recommended_style = 0,
+  },
+}
